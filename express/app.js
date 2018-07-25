2760c336b6e3813584fd44ddf34945209da7a7b1
Pete <peter.adam.bialecki@gmail.com>
2018-07-24 19:39:24 -0500

    Created new 'hey' page.

--- express/app.js
+++ express/app.js
@@ -6,4 +6,10 @@ app.get('/', (req, res) => {
     res.send('<h1>Generating stuff...</h1>');
 });
 
-app.listen(3000);
+app.get('/hey', (req, res) => {
+    res.send('<h1>Glad you could make it!</h1>');
+});
+
+app.listen(3000, () => {
+    console.log('Testing Express stuff...');
+});
