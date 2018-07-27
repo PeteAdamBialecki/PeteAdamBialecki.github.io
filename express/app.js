69cfa6b3be5d2d4f2c0478d2f35b910f9f03dca3
Pete <peter.adam.bialecki@gmail.com>
2018-07-27 17:52:35 -0500

    Installed bodyParser. Added GET and POST for new page and input form.

--- express/app.js
+++ express/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: false}));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
@@ -10,6 +11,15 @@ app.get('/', (req, res) => {
 
 app.get('/cards', (req, res) => {
     res.render('card', {prompt: "What was Buddha's original name?", hint: "It's indian..."});
+});
+
+app.get('/hello', (req, res) => {
+    res.render('hello',);
+});
+
+app.post('/hello', (req, res) => {
+    console.dir(req.body);
+    res.render('hello',);
 });
 
 app.listen(3000, () => {
