9a8408aba30e902cc4cd0398d92bf841833e01de
Pete <peter.adam.bialecki@gmail.com>
2018-08-20 21:57:26 -0500

    Added additional data sets and simplified code.

--- d3js/script.js
+++ d3js/script.js
@@ -1,10 +1,62 @@
-var w = 300;
-var h = 100;
-var padding = 2;
-var data = [5, 10, 14, 52, 63, 23, 15, 40, 30];
+var w = 1000;
+var h = 500;
+var padding = 5;
+var dataset = [63, 23, 15, 40, 16, 34, 70, 5, 10, 14,  40, 30, 63, 23, 15, 40, 16, 34, 70, 52, 63, 23, 15, 40, 30, 63, 23, 15, 40, 16, 34, 70, 42, 14, 5, 3, 65, 52, 41];
 var svg = d3.select("body").append("svg")
     .attr("width", w)
     .attr("height", h);
+
+svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+        .attr({
+            x: function(d, i) {return i * (w / dataset.length);},
+            y: function(d) {return h - (d*4);},
+            width: w / dataset.length - padding,
+            height: function(d) {return d*4;},
+            fill: function(d) {return "rgb(" + (d*5) + ", 0, 0)";}
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
