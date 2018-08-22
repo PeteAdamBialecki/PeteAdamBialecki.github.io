db2eed22e3a0aa14f12296513df36fffa2143dcf
Pete <peter.adam.bialecki@gmail.com>
2018-08-21 19:56:41 -0500

    Completed JavaScript section for bar chart.

--- d3js/script.js
+++ d3js/script.js
@@ -1,7 +1,9 @@
+
+// BAR CHART
 var w = 1000;
 var h = 500;
 var padding = 5;
-var dataset = [63, 23, 15, 40, 16, 34, 70, 5, 10, 14,  40, 30, 63, 23, 15, 40, 16, 34, 70, 52, 63, 23, 15, 40, 30, 63, 23, 15, 40, 16, 34, 70, 42, 14, 5, 3, 65, 52, 41];
+var dataset = [63, 23, 15, 40, 16, 34, 70, 5, 10, 14,  40, 30, 63, 23, 15, 40, 16, 34, 70, 52, 63, 23, 15, 40, 30, 63, 23, 15, 40, 16, 34, 70, 42, 14, 5, 7, 65, 52, 41];
 var svg = d3.select("body").append("svg")
     .attr("width", w)
     .attr("height", h);
@@ -11,6 +13,7 @@ function colorPicker(v) {
     else if (v>20) {return "greenflat";}
 }
 
+// Coloring the bar chart.
 svg.selectAll("rect")
     .data(dataset)
     .enter()
@@ -23,39 +26,20 @@ svg.selectAll("rect")
             fill: function(d) {return colorPicker(d);}
         });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Bar Labeling.
+svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function(d) { return d; })
+    .attr({
+        "text-anchor": "middle",
+        x: function( d, i ) {return i * (w / dataset.length) + (w / dataset.length - padding) / 2;},
+    y: function(d) { return h - (d*4)+14; },
+    "font-family": "sans-serif",
+    "font-size": 12,
+    "fill": "#999"
+});
 
 
 
