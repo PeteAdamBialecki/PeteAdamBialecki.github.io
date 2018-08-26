5d47a9eb3173837e8300bd9a428086acdaddf2bd
Pete <peter.adam.bialecki@gmail.com>
2018-08-26 12:53:19 -0500

    Created templates for tooltips, transitons, and reorganized JavaScript.

--- d3js/template-scripts.js
+++ d3js/template-scripts.js
@@ -24,21 +24,28 @@ svg.selectAll("rect")
     width: w / dataset.length - padding,
     height: function(d) {return d*4;},
     fill: function(d) {return colorPicker(d);}
-  });
+  })
+  .on("mouseover", function(d){
+    
+    svg.append("text")
+      .text(d)
+      .attr({
+        "text-anchor": "middle",
+        x: parseFloat(d3.select(this).attr("x")) +
+          parseFloat(d3.select(this).attr("width")/2),
+        y: parseFloat(d3.select(this).attr("y"))+12,
+        "font-family": "sans-serif",
+        "font-size": 12,
+        "fill": "#ffffff",
+        id: "tooltip"
+      });
+  
+})
 
-svg.selectAll("text")
-  .data(dataset)
-  .enter()
-  .append("text")
-  .text(function (d) { return d; })
-  .attr({
-    "text-anchor": "middle",
-    x: function(d, i) {return i * (w / dataset.length)+(w / dataset.length - padding) / 2;},
-    y: function(d) { return h - (d*4)+14; },
-    "font-family": "sans-serif",
-    "font-size": 12,
-    "fill": "#ffffff"
-  });
+
+
+
+  .on("mouseout", function(){d3.select("#tooltip").remove()});
 
 
 
