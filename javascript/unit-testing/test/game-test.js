7fb2232c67dcaf71e336bbec02686b86739b673b
Pete <peter.adam.bialecki@gmail.com>
2018-09-16 10:17:13 -0500

    Created pending test function.

---
+++ javascript/unit-testing/test/game-test.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('GAME INSTANCE FUNCTIONS', function () {
+    describe('checkGameStatus', function () {
+        it('Should tell me when the game is over.');
+    });
+});
