e34961e4d421e28e6205fbf991bdf276f5cd7b74
Pete <peter.adam.bialecki@gmail.com>
2018-10-24 20:04:37 -0500

    Created d3.chart.table function.

---
+++ d3js/5-interactive-data-visualization/extra/main/code/extra/table.js
@@ -0,0 +1,35 @@
+if (!d3.chart) d3.chart = {};
+
+d3.chart.table = function() {
+
+var table = display.append("table")
+var rows = table.selectAll("tr.row")
+    .data(data)
+var rowsEnter = rows.enter()
+    .append("tr").classed("row", true)
+
+rowsEnter.append("td")
+    .text(function(d) {return d.data.score})
+rowsEnter.append("td")
+        .append("a")
+        .attr({
+        href: function(d) {return d.data.url}
+    })
+        .append("img")
+        .attr({
+        src: function(d) {return d.data.thumbnail}
+    })
+
+rowsEnter.append("td")
+        .append("a")
+        .attr({
+        href: function(d) {return d.data.url}
+    })
+        .text(function(d) {return d.data.title})
+
+rowsEnter.append("td")
+        .text(function(d) {return d.data.downs})
+
+rowsEnter.append("td")
+        .text(function(d) {return d.data.ups})
+}
