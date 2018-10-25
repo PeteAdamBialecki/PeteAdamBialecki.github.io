bdf989b82a690b2c8a1a8f6ab7acc4c3f77b470d
Pete <peter.adam.bialecki@gmail.com>
2018-10-24 21:43:39 -0500

    Updated tables.js.

--- d3js/5-interactive-data-visualization/extra/main/code/extra/table.js
+++ d3js/5-interactive-data-visualization/extra/main/code/extra/table.js
@@ -1,35 +1,60 @@
-if (!d3.chart) d3.chart = {};
+if(!d3.chart) d3.chart = {};
 
 d3.chart.table = function() {
+  var data;
+  var div;
 
-var table = display.append("table")
-var rows = table.selectAll("tr.row")
-    .data(data)
-var rowsEnter = rows.enter()
-    .append("tr").classed("row", true)
+  function chart(container) {
+    div = container;
+    var table = div.append("table")
+    update();
+  }
 
-rowsEnter.append("td")
-    .text(function(d) {return d.data.score})
-rowsEnter.append("td")
-        .append("a")
-        .attr({
-        href: function(d) {return d.data.url}
+  function update() {
+    var table = div.select("table")
+    var trs = table.selectAll("tr.data")
+      .data(data, function(d) { return d.data.id })
+
+    var enter = trs.enter()
+      .append("tr")
+      .classed("data", true)
+
+    trs.exit().remove();
+
+    enter.append("td")
+      .text(function(d) { return d.data.score })
+    enter.append("td")
+      .append("img")
+      .attr("src", function(d) { return d.data.thumbnail})
+    enter.append("td")
+      .append("a")
+      .attr("href", function(d) { return d.data.url })
+      .text(function(d) { return d.data.title })
+    enter.append("td")
+      .text(function(d) { return d.data.ups })
+    enter.append("td")
+      .text(function(d) { return d.data.downs })
+  }
+
+  chart.update = update;
+
+  chart.highlight = function(data) {
+    div.selectAll("tr").selectAll("td")
+    .style({
+      "background-color": ""
     })
-        .append("img")
-        .attr({
-        src: function(d) {return d.data.thumbnail}
+    var trs = div.selectAll("tr")
+      .data(data, function(d) { return d.data.id });
+    trs.selectAll("td").style({
+      "background-color": "orange",
+      border: "1px solid black"
     })
+  }
 
-rowsEnter.append("td")
-        .append("a")
-        .attr({
-        href: function(d) {return d.data.url}
-    })
-        .text(function(d) {return d.data.title})
-
-rowsEnter.append("td")
-        .text(function(d) {return d.data.downs})
-
-rowsEnter.append("td")
-        .text(function(d) {return d.data.ups})
+  chart.data = function(value) {
+    if(!arguments.length) return data;
+    data = value;
+    return chart;
+  }
+  return chart;
 }
