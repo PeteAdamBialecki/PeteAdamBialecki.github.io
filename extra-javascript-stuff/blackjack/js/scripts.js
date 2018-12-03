811bdf5ac241ad99a9bb5b186ef315fff3cc246a
Pete <peter.adam.bialecki@gmail.com>
2018-12-02 19:54:14 -0600

    Added additional game variables and updated newGameButton().

--- extra-javascript-stuff/blackjack/js/scripts.js
+++ extra-javascript-stuff/blackjack/js/scripts.js
@@ -3,20 +3,38 @@ let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
 let values = ['Ace', 'King', 'Queen', 'Jack', 
             'Ten', 'Nine', 'Eight', 'Seven', 'Six',
             'Five', 'Four', 'Three', 'Two'];
-let textArea = document.getElementById('text-area');
-let newGameButton = document.getElementById('new-game-button');
-let hitButton = document.getElementById('hit-button');
-let stayButton = document.getElementById('stay-button');
+let textArea = document.getElementById('text-area'),
+    newGameButton = document.getElementById('new-game-button'),
+    hitButton = document.getElementById('hit-button'),
+    stayButton = document.getElementById('stay-button');
+
+let gameStarted = false,
+    gameOver = false,
+    playerWon = false,
+    dealerCards = [],
+    playerCards = [],
+    dealerScore = 0,
+    playerScore = 0,
+    deck = [];
 
 hitButton.style.display = 'none';
 stayButton.style.display = 'none';
 
 newGameButton.addEventListener('click', function() {
-    textArea.innerText = 'Game started...';
+    gameStarted = true;
+    gameOver = false;
+    playerWon = false;
+    
+    deck = createDeck();
+    shuffleDeck(deck);
+    dealerCards = [ getNextCard(), getNextCard() ];
+    playerCards = [ getNextCard(), getNextCard() ];
+    
     newGameButton.style.display = 'none';
     hitButton.style.display = 'inline';
     stayButton.style.display = 'inline';
-});
+    showStatus();
+  });
 
 function createDeck() {
     let deck = [];
