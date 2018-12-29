33d0cd03bf314a6b2c1d8e3d708492860e1a4e46
Lucas Groenendaal <groenendaal92@gmail.com>
2018-12-29 18:12:04 -0500

    Added random survival function.

--- extra-javascript-stuff/quick-start/volume-1/scripts/building.js
+++ extra-javascript-stuff/quick-start/volume-1/scripts/building.js
@@ -46,3 +46,13 @@ var weaponList = ["shovel", "crossbow", "baseball bat", "rubber chicken"];
 
 var weapon = weaponList[ randomNumber( weaponList.length - 1) ];
 alert("Being that it is the zombie apocalypse, you decide to search for a weapon first. After surveying your surroundings you notice and grab a " + weapon + ".");
+
+alert("Suddenly a zombie bursts through the door!  You ready your " + weapon + " and advance towards the zombie.");
+
+var survival =  randomNumber(2);
+
+if(survival === 0 ){
+  alert("The zombie bites you.  You lose!!!");
+} else if ( survival > 0 ){
+  alert("You kill the zombie with your" + " " + weapon + ". You win!");
+}
