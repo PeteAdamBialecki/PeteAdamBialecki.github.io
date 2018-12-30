4a008b61e09bacad256d5c9b6ea798b0f1775353
Lucas Groenendaal <groenendaal92@gmail.com>
2018-12-30 16:04:29 -0500

    Created template for methods.

---
+++ extra-javascript-stuff/fundamentals/scripts/method.js
@@ -0,0 +1,12 @@
+var myCoffee = {
+    flavor: "espresso",
+    temperature: "piping hot",
+    ounces: 100,
+    milk: true,
+    reheat: function() {
+        if(myCoffee.temperature === "cold") {
+            
+        }
+    }
+};
+
