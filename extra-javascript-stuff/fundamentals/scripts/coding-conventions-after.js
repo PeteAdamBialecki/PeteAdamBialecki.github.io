134e91a7e6f09df9c714ce9e4c1fa38d681f0f32
Pete <peter.adam.bialecki@gmail.com>
2019-01-01 13:12:19 -0600

    Coding conventions linted.

--- extra-javascript-stuff/fundamentals/scripts/coding-conventions-after.js
+++ extra-javascript-stuff/fundamentals/scripts/coding-conventions-after.js
@@ -8,7 +8,9 @@ alert, confirm, console, prompt
 alert("Hello World!");
 
 function lameJoke() {
-  randomNumber = Math.round(Math.random() * 2);
+    // "use strict" is used when decalring a variable like 'randomNumber' below.
+    "use strict";
+    var randomNumber = Math.round(Math.random() * 2);
     if (randomNumber === 1) {
         alert("How many tickles does it take to make an octopus laugh? Ten tickles.");
     } else if (randomNumber === 2) {
