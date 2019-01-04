8c4bb694c97f469aacb273fa618f55e286046f59
Pete <peter.adam.bialecki@gmail.com>
2019-01-03 20:19:27 -0600

    Added class constructor.

--- extra-javascript-stuff/fundamentals/scripts/script.js
+++ extra-javascript-stuff/fundamentals/scripts/script.js
@@ -1,1 +1,24 @@
-// Start coding here
+'use strict';
+
+//function Cat(name, color ) {
+//  this.name = name
+//  this.color = color
+//}
+
+//var cat = new Cat('Fluffy', 'White');
+
+class Cat {
+  constructor(name, color) {
+    this.name = name
+    this.color = color
+  }
+  
+  speak() {
+    display('Meeooow')
+  }
+}
+
+var cat = new Cat('Fluffy', 'White')
+
+display(cat)
+cat.speak()
