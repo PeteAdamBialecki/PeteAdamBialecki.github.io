ff0d67bb2d0d7d8c13dc1594e65ac14801f1b3c8
Pete <peter.adam.bialecki@gmail.com>
2019-01-27 12:10:05 -0600

    Task.js and and main.js linked and updated.

--- extra-javascript-stuff/design-patterns/node-demos/app.js
+++ extra-javascript-stuff/design-patterns/node-demos/app.js
@@ -1,12 +1,14 @@
-"use strict";
-var main = require('./task')
+'use strict';
+
+var main = require('./main');
+
 const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
-    });
+});
 
-    app.listen(3000, function () {
+app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
-    });
+});
--- extra-javascript-stuff/design-patterns/node-demos/task.js
+++ extra-javascript-stuff/design-patterns/node-demos/task.js
@@ -1,38 +1,15 @@
 var Task = function (name) {
     this.name = name;
-    this.completed = false;
-
-    this.complete = function () {
-        console.log('Completing task: ' + this.name);
-        this.complete = true;
-    };
-
-    this.save= function() {
-        console.log('Saving task: ' + this.name);
-    };
+    this.completed = false; 
 };
 
-var task1 = new Task('Create a demo for constructors.');
-var task2 = new Task('Create a demo for modules.');
-var task3 = new Task('Create a demo for singletons.');
-var task4 = new Task('Create a demo for prototypes.');
+Task.prototype.complete = function() {
+    console.log('Completing Task: ' + this.name);
+    this.completed = true;
+};
 
-task1.complete();
-task2.save();
-task3.save();
-task4.save();
+Task.prototype.save = function() {
+    console.log('Saving Task: ' + this.name);
+};
 
-
-
-
-
-// Task.prototype.comple = function() {
-//     console.log('Completing Task: ' + this.name);
-//     this.completed = true;
-// };
-
-// Task.prototype.save = function() {
-//     console.log('Saving Task: ' + this.name);
-// }
-
-// module.exports = Task;
+module.exports = Task;
