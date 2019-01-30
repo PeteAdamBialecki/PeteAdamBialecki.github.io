bb2844841064225aa0b809873306c85b42bd59cc
Pete <peter.adam.bialecki@gmail.com>
2019-01-29 18:54:53 -0600

    Added sample code for code before using a JavaScript decorator pattern.

---
+++ extra-javascript-stuff/design-patterns/node-demos/02-structural-design-patterns/01-decorator/task.js
@@ -0,0 +1,17 @@
+var Task = function (name) {
+    this.name = name;
+    this.completed = false; 
+};
+
+Task.prototype.complete = function() {
+    console.log('Completing Task: ' + this.name);
+    this.completed = true;
+};
+
+Task.prototype.save = function() {
+    console.log('Saving Task: ' + this.name);
+};
+
+var myTask = new Task('Legacy Task');
+myTask.complete();
+myTask.save();
