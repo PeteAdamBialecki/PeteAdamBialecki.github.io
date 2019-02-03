f9c63146b98f1b433ebe617c4d2fe435c8a3d4c8
Pete <peter.adam.bialecki@gmail.com>
2019-02-03 15:58:09 -0600

    Added removeObserver.

--- extra-javascript-stuff/design-patterns/node-demos/03-behavioral-design-patterns/01-observer-pattern/main.js
+++ extra-javascript-stuff/design-patterns/node-demos/03-behavioral-design-patterns/01-observer-pattern/main.js
@@ -31,13 +31,30 @@ ObserverList.prototype.get = function (index) {
 ObserverList.prototype.count = function (obj) {
     return this.observerList.length;
 };
+ObserverList.prototype.removeAt = function (index) {
+    this.observerList.splice(index, 1);
+};
+ObserverList.prototype.indexOf = function (obj, startIndex) {
+    var i = startIndex;
 
+    while (i < this.observerList.length) {
+        if (this.observerList[i] === obj) {
+            return i;
+        }
+        i++;
+    }
+
+    return -1;
+} 
 var ObservableTask = function (data) {
     Task.call(this, data);
     this.observers = new ObserverList();
 };
 ObservableTask.prototype.addObserver = function (observer) {
     this.observers.add(observer);
+};
+ObservableTask.prototype.removeObserver = function (observer) {
+    this.observers.removeAt( this.observers.indexOf( observer, 0 ) );
 };
 ObservableTask.prototype.notify = function (context) {
     var observerCount = this.observers.count();
@@ -59,4 +76,8 @@ task1.addObserver(not.update);
 task1.addObserver(ls.update);
 task1.addObserver(audit.update);
 
+task1.save();
+console.log(' ');
+task1.removeObserver(audit.update);
+console.log(' ');
 task1.save();
