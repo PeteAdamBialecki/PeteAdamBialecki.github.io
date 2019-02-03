f67347fb140291c9eacc775c29db38bf345dfa8d
Pete <peter.adam.bialecki@gmail.com>
2019-02-03 10:52:23 -0600

    Added more sample code for flyweight experimenting.

--- extra-javascript-stuff/design-patterns/node-demos/02-structural-design-patterns/03-flyweight-pattern/task.js
+++ extra-javascript-stuff/design-patterns/node-demos/02-structural-design-patterns/03-flyweight-pattern/task.js
@@ -13,5 +13,39 @@ function TaskCollection() {
         tasks[data.name] =
             new Task(data);
                 count++;
-    }
+    };
+    var get = function(name) {
+        return task[name];
+    };
+    var getCount = function() {
+        return count;
+    };
+    return {
+        add: add,
+        get: get,
+        getCount: getCount
+    };
 }
+
+var tasks = new TaskCollection();
+
+var projects = ['none', 'courses', 'training', 'project'];
+var priorities = [1,2,3,4,5];
+var users = ['Jon', 'Erica', 'Amanda', 'Nathan'];
+var completed = [true, false];
+var initialMemory = process.memoryUsage().heapUsed;
+
+for (var i = 0; i < 10000; i++) {
+    task.add({
+        name: 'task' + i,
+        priority: priorities[Math.floor((Math.random() * 5))],
+        project: projets[Math.floor((Math.random() * 4))],
+        user: users[Math.floor((Math.random() * 4))],
+        completed: cmpleted[Math.floor((Math.random() * 2))]
+    });
+};
+
+var afterMemory = process.memoryUsage().heapUsed;
+console.log('Used memory: ' + (afterMemory - initialMemory) / 1000000);
+
+console.log("Tasks generated: " + task.getCount());
