5c55549cf1c19f3257a6914e080baa57dbb54a3b
Pete <peter.adam.bialecki@gmail.com>
2019-02-04 20:13:18 -0600

    Starting code for command pattern experimenting.

---
+++ extra-javascript-stuff/design-patterns/node-demos/03-behavioral-design-patterns/03-command-pattern/taskRepo.js
@@ -0,0 +1,11 @@
+var repo = {
+    get: function (id) {
+        console.log('Getting task ' + id);
+        return {
+            name: 'new task from db'
+        }
+    },
+    save: function (task) {
+        console.log('Saving ' + task.name + ' to the db');
+    }
+}
