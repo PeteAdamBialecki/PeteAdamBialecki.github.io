aa6a7505665aa10de02c1a20798792230dc0f881
Pete <peter.adam.bialecki@gmail.com>
2019-02-21 21:07:31 -0600

    Completed task four.

--- extra-javascript-stuff/practice/00-jquery-collections/js/app.js
+++ extra-javascript-stuff/practice/00-jquery-collections/js/app.js
@@ -2,7 +2,6 @@
   //  Append a new span element containing the word "Featured" to the list item
   //  Add a class of "featured" to the new span element.
 //2. Add a class of "new" to the last item in the list. You can traverse to the last item or find a jQuery method that helps you select the last item in the list. 
-
 //3. Add an attribute to each link so that all anchor tags open in a new tab.
 
 //4. Select and print the index and text of each anchor tag to the console
@@ -16,3 +15,7 @@ $("#restaurantList span").addClass('featured');
 $('#restaurantList li:last-child').addClass('new');
 //3:
 $('a').attr( "target", "_blank" );
+// 4:
+$('a').each(function(index, item){
+    console.log(index, $(item).text());
+  });
