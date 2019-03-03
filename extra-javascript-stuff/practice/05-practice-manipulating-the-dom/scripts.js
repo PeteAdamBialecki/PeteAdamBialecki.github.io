42a485dbfc292b0f600779b3f86d00735dc0d4ac
pbialecki_augeofi <pbialecki@augeofi.com>
2019-03-03 11:28:47 -0600

    Created and added delete button.

--- extra-javascript-stuff/practice/05-practice-manipulating-the-dom/scripts.js
+++ extra-javascript-stuff/practice/05-practice-manipulating-the-dom/scripts.js
@@ -1,29 +1,36 @@
 // 1: Set the text of the <h1> element
 // 2: Set the color of the <h1> to a different color
-let title = "My Activities List",
+const title = 'My Activities List',
     titleText = document.querySelector('h1');
     titleText.innerHTML = title;
-    titleText.style.color = "teal";
+    titleText.style.color = 'teal';
 
 // 3: Set the content of the '.desc' paragraph. The content should include at least one HTML tag.
-document.getElementsByClassName("desc")[0].innerHTML = "<p>A list of fun things I want to do.</p>";
+document.getElementsByClassName('desc')[0].innerHTML = '<p>A list of fun things I want to do.</p>';
 
 // 4: Set the class of the <ul> to 'list'
-var element = document.querySelector('ul');
-element.classList.add('list');
+const element = document.querySelector('ul');
+    element.classList.add('list');
 
 // 5: Create a new list item and add it to the <ul>
-var lastText = document.createElement("li");
-var lastLi = document.createElement("input");
-lastText.innerHTML = '<input> Biking </input>';
-document.querySelector('ul').appendChild(lastText);
+const lastText = document.createElement('li');
+const lastLi = document.createElement('input');
+    lastText.innerHTML = '<input> Biking </input>';
+        document.querySelector('ul').appendChild(lastText);
 
 // 6: Change all <input> elements from text fields to checkboxes
-var list = document.getElementsByTagName('input');
-for (var i=0; i<list.length; i++)
-list[i].setAttribute("type", "checkbox");
+const list = document.getElementsByTagName('input');
+    for (let i=0; i<list.length; i++)
+        list[i].setAttribute('type', 'checkbox');
 
 // 7: Create a <button> element, and set its text to 'Delete'
+const deleteContainer = document.getElementsByClassName('extra');
+const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'Delete';
+        document.getElementsByClassName('extra')[0].appendChild(deleteButton);
+
+
+
 // Add the <button> inside the '.extra' <div>
 
 
