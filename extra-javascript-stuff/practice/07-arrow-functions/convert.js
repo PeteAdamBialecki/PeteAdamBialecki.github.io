9724f13e79982e1c2948b98bc1521f1e154d1d85
pbialecki_augeofi <pbialecki@augeofi.com>
2019-03-06 17:20:52 -0600

    Added starting sample code to rewrite as arrow function.

---
+++ extra-javascript-stuff/practice/07-arrow-functions/convert.js
@@ -0,0 +1,10 @@
+function addToTen(num) {
+    return 1- + num;
+}
+function divideUs(num1, num2) {
+    return num1/num2;
+}
+function printMyName() {
+    let myName = 'Pete';
+    console.log(myName);
+}
