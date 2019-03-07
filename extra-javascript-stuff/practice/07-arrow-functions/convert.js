3d1ce0d72ed41232f642b66518bb4b6254d1d603
pbialecki_augeofi <pbialecki@augeofi.com>
2019-03-07 15:49:13 -0600

    Created first pass for converting to arrow functions.

--- extra-javascript-stuff/practice/07-arrow-functions/convert.js
+++ extra-javascript-stuff/practice/07-arrow-functions/convert.js
@@ -1,10 +1,31 @@
-function addToTen(num) {
-    return 1- + num;
-}
-function divideUs(num1, num2) {
-    return num1/num2;
-}
-function printMyName() {
-    let myName = 'Pete';
-    console.log(myName);
-}
+
+console.log('Conneted...');
+
+// #1
+// Function before arrow converting:
+    // function addToTen(num) {
+    //     return 10 + num;
+    // }
+// Same function from above as arrow:
+    let addToTen = (num) => 10 + num
+
+// #2
+// Function before arrow converting:
+    // function divideUs(num1, num2) {
+    //     return num1/num2;
+    // }
+// Same function from above as arrow:
+    let divideUs = (num1, num2) => num1/num2
+
+// #3
+// Function before arrow converting:
+    // function printMyName() {
+    //     let myName = 'Pete';
+    //     console.log(myName);
+    // }
+// Same function from above as arrow:
+    let printMyName = (myName) => console.log('Pete')
+
+console.log(addToTen(5));
+console.log(divideUs(5,5));
+printMyName();
