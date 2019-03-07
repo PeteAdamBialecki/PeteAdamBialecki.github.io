bd9857adfa94780bbb480ba72fd15d430090ee03
pbialecki_augeofi <pbialecki@augeofi.com>
2019-03-07 16:15:21 -0600

    Added 'debug' code for arrow refactoring.

--- extra-javascript-stuff/practice/07-arrow-functions/refactor.js
+++ extra-javascript-stuff/practice/07-arrow-functions/refactor.js
@@ -1,29 +1,54 @@
 
-// #1
-// Original:
-    // const multiplyByTen = (num) => {
-    //     return 10 * num;
-    // }
-// With arrow function:
+// // My Version
+// // #1
+// // Original:
+//     // const multiplyByTen = (num) => {
+//     //     return 10 * num;
+//     // }
+// // With arrow function:
+//     const multiplyByTen = (num) => 10 * num
+
+// // #2
+// // Original:
+//     const printToTen = () => {
+//         for (let i = 1; i <= 10; i++) {
+//             console.log(i);
+//         }
+//     }
+// // With arrow function:
+
+
+// // #3
+// // Original:
+//     // const addUs = (num1, num2) => {
+//     //     console.log(num1 + num2);
+//     // }
+// // With arrow function
+//     const addUs = (num1, num2) => console.log(num1 + num2)
+
+
+// console.log(multiplyByTen(7));
+// addUs(9,8);
+
+
+
+
+
+
+
+
+// Second Version:
     const multiplyByTen = (num) => 10 * num
 
-// #2
-// Original:
-    // const printToTen = () => {
-    //     for (let i = 1; i <= 10; i++) {
-    //         console.log(i);
-    //     }
-    // }
-// With arrow function:
+    const printToTen = () => {
+        for (let i = 1; i <= 10; i++) {
+            console.log(i);
+        }
+    }
 
-// #3
-// Original:
-    // const addUs = (num1, num2) => {
-    //     console.log(num1 + num2);
-    // }
-// With arrow function
     const addUs = (num1, num2) => console.log(num1 + num2)
 
 
 console.log(multiplyByTen(7));
+printToTen();
 addUs(9,8);
