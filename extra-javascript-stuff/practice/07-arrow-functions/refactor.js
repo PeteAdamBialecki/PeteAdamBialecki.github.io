7ad6b086e2fe8a99ad3e2abd8d9dc972ff0b66f0
pbialecki_augeofi <pbialecki@augeofi.com>
2019-03-07 16:02:00 -0600

    Added additional code to be refactored.

---
+++ extra-javascript-stuff/practice/07-arrow-functions/refactor.js
@@ -0,0 +1,19 @@
+
+const multiplyByTen = (num) => {
+    return 10 * num;
+}
+
+const printToTen = () => {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+const addUs = (num1, num2) => {
+    console.log(num1 + num2);
+}
+
+
+console.log(addToTen(5));
+console.log(divideUs(5,5));
+printMyName();
