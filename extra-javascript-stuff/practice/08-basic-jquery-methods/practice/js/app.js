ff25939c9b14ea0b45df4425e3deac994e717edc
pbialecki_augeofi <pbialecki@augeofi.com>
2019-03-09 11:42:02 -0600

    Completed practice with jQuery methods.

--- extra-javascript-stuff/practice/08-basic-jquery-methods/practice/js/app.js
+++ extra-javascript-stuff/practice/08-basic-jquery-methods/practice/js/app.js
@@ -8,15 +8,17 @@ $('.review p').html('There is a lot of <b>STUFF</b> here.');
 $('h1 span').text('AWESOME');
 // 5. Create an animation for the paragraph with the id of "flashMessage." Chain an animation that 
 $('#flashMessage').slideDown('slow').delay(2000).slideUp('slow');
-  // slides the message down for 1 second
-  // delays the message for 2 seconds 
-  // slides the message up for 1 second 
-
+    // slides the message down for 1 second
+    // delays the message for 2 seconds 
+    // slides the message up for 1 second 
 // 6. Inside the click handler, create a working restaurant review form.
 $('#submitBtn').click(function(){
-  //  a. Use the val() method to get what's been entered into the restaurant name and restaurant review form input fields. Save both values to variables.
-  //  b. Insert the restaurant name value into the #newRestaurant h3 tag 
-  //  c. Insert the restaurant review value into the #newRestaurant paragraph tag
+    let restaurantTitle = $('#restaurantNameInput').val(); 
+    let restaurantReview = $('#restaurantReviewInput').val(); 
+        $('#newRestaurant h3').text(restaurantTitle);
+        $('#newRestaurant p').text(restaurantReview);
+        $('#restaurantNameInput, #restaurantReviewInput').val('');
+        //  a. Use the val() method to get what's been entered into the restaurant name and restaurant review form input fields. Save both values to variables.
+        //  b. Insert the restaurant name value into the #newRestaurant h3 tag 
+        //  c. Insert the restaurant review value into the #newRestaurant paragraph tag
 });
-
-
