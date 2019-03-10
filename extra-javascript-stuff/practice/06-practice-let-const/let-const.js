cebfa4d20f32afcee91a12c7e75b2235dc8f12b8
Pete <peter.adam.bialecki@gmail.com>
2019-03-10 14:12:24 -0500

    Updating branch.

--- extra-javascript-stuff/practice/06-practice-let-const/let-const.js
+++ extra-javascript-stuff/practice/06-practice-let-const/let-const.js
@@ -1,7 +1,7 @@
 // 1) 	Write the code for function addNewTeachers() found on line 25. This function should receive parameter newTeachers, defined on line 52, and add the new teachers to the treehouseTeachers variable. 
- 
+
 // 2)	Find function printTreehouseSummary(). There is something you need to fix in this function so the console.log() on the final line of the function outputs the correct number of JavaScript courses and teachers.
- 
+
 // 3)	At this point, attempt to run your code by typing node let_const.js in your workspace console and hitting enter. You should receive an error message when you do this. Use the clues in this error message to fix the program so it runs.
 
 const teachers = [
@@ -17,7 +17,7 @@ const courses = ['Introducing JavaScript',
 				'Getting Started with ES2015',
 				'JavaScript and the DOM',
 				'DOM Scripting By Example'];
-			
+
 
 var i = courses.length;
 
@@ -29,11 +29,11 @@ function addNewTeachers(newTeachers) {
 
 function printTreehouseSummary() {
 	// TODO: fix this function so that it prints the correct number of courses and   teachers 
-	
+
   for (var i = 0; i < teachers.length; i++) {
 	 console.log(`${teachers[i].name} teaches ${teachers[i].topicArea}`);
 	}
-	
+
   console.log(`Treehouse has ${i} JavaScript courses, and ${teachers.length} Javascript teachers`);
 }
 
