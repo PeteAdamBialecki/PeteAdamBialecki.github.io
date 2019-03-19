37ad16cf514e3e1d7c302b66d5a7364b910836ff
pbialecki_augeofi <pbialecki@augeofi.com>
2019-03-19 17:22:19 -0500

    Started practice of creating robot in a maze exercise. Maze function and prototype created.

---
+++ extra-javascript-stuff/04-practice/12-oop-javascript/my-version/js/maze.js
@@ -0,0 +1,32 @@
+'use strict';
+
+function maze(width, height) {
+    this.width = width;
+    this.height = height;
+
+    this.startX = null;
+    this.startY = null;
+    this.startOrientation = null;
+    this.endX = null;
+    this.endY = null;
+    this.spaces = [];
+
+    var x;
+    for (x = 1; x <= width; x += 1) {
+        this.spaces[x] = [];
+        for (y = 1; y <= height; y += 1) {
+            this.spaces[x][y] = "(" + x + "," + y + ")";
+        }
+    }
+}
+
+maze.prototype.setStart = function(x, y, orientation) {
+    this.startX = x;
+    this.startY = y;
+    this.startOrientation = orientation;
+}
+
+maze.prototype.setEnd = function (x, y) {
+    this.endX = x;
+    this.endY = y;
+}
