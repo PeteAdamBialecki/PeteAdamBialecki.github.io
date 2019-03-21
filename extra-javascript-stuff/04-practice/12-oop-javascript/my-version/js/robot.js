7d9e98a6603b6da92f0ba863a542a330d476efa9
pbialecki_augeofi <pbialecki@augeofi.com>
2019-03-20 20:33:33 -0500

    Created robot for maze object.

---
+++ extra-javascript-stuff/04-practice/12-oop-javascript/my-version/js/robot.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function Robot() {
+    this.x = null;
+    this.y = null;
+    this.orientation = null;
+    this.maze = null;
+}
+
+Robot.prototype.setMaze = function(maze) {
+    this.maze = maze;
+    this.x = maze.startX;
+    this.y = maze.startY;
+    this.orientation = maze.startOrientation;
+}
