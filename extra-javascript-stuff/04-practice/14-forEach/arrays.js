809f7f575c688eabd9df12258030c3e3bad21044
pbialecki_augeofi <pbialecki@augeofi.com>
2019-04-09 17:23:31 -0500

    Added example and notes on array iteration with forEach().

---
+++ extra-javascript-stuff/04-practice/14-forEach/arrays.js
@@ -0,0 +1,39 @@
+console.log(" ");
+console.log("Connected...");
+console.log("----------------------------");
+
+
+// `for` vs forEach() Example #1
+
+const fruits = ['apple', 'pear', 'cherry'];
+
+    // Version #1: for loop
+    for (let i = 0; i < fruits.length; i += 1) {
+        console.log(fruits[i]);
+    }
+
+    // Version #2: for each
+    fruits.forEach(fruit => console.log(fruit));
+
+        // Easier to read, bugs are easier to avoid, infinite loops impossible, avoids incrementing mistakes, and prevents setting wrong conditions.
+
+        // Bad: you can't break out of a loop early. Use for or while loops instead.
+
+console.log("----------------------------");
+
+// `for` vs forEach() Example #2
+let capitilizedFruits = [];
+    fruits.forEach(fruit => {
+        let capitilizedFruit = fruit.toUpperCase();
+        capitilizedFruits.push(capitilizedFruit);
+    });
+
+console.log(capitilizedFruits);
+
+console.log("----------------------------");
+
+// `for` vs forEach() Example #3
+const prices = [6.75, 3.10, 4.00, 8.12];
+let addedPrices = 0;
+    prices.forEach(price => addedPrices += price);
+    console.log(addedPrices);
