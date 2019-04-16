713dea4790a6199901fffa20fc6e46cb687b9b97
pbialecki_augeofi <pbialecki@augeofi.com>
2019-04-15 20:18:45 -0500

    Sample for map() and reduce().

--- extra-javascript-stuff/04-practice/14-forEach/arrays.js
+++ extra-javascript-stuff/04-practice/14-forEach/arrays.js
@@ -465,9 +465,46 @@ console.log(" ");
         [ "Portia", "Pam", "Philip" ]
     ];
     let flattenedCustomerNames;
-    
+
     // flattenedCustomerNames should be: ["John", "Sandy", "Tyrone", "Elizabeth", "Penny", "Barry", "Wanda", "Jamal", "Hayden", "Portia", "Pam", "Philip"]
     // Write your code below
-    
+
     flattenedCustomerNames = customerNames.reduce((arr, innerNames) => [...arr, ...innerNames], []);
-            
+
+console.log("--------------------------------------------------------");
+console.log("Filter and Map Example #9:");
+console.log(" ");
+
+const users = [
+    {
+      name: 'Samir',
+      age: 27,
+      favoriteBooks:[
+        {title: 'The Iliad'},
+        {title: 'The Brothers Karamazov'}
+      ]
+    },
+    {
+      name: 'Angela',
+      age: 33,
+      favoriteBooks:[
+        {title: 'Tenth of December'},
+        {title: 'Cloud Atlas'},
+        {title: 'One Hundred Years of Solitude'}
+      ]
+    },
+    {
+      name: 'Beatrice',
+      age: 42,
+      favoriteBooks:[
+        {title: 'Candide'}
+      ]
+    }
+  ];
+  
+      // Result: ['The Iliad', 'The Brothers Karamazov', 'Tenth of December', 'Cloud Atlas', 'One Hundred Years of Solitude', 'Candide'];
+    const books = users
+        .map(user => user.favoriteBooks.map(book => book.title))
+        .reduce((arr, titles) => [...arr, ...titles], []);
+
+    console.log(books);
