c762b968860cdda42384232ca1fca43863e71eb8
pbialecki_augeofi <pbialecki@augeofi.com>
2019-04-16 18:02:48 -0500

    Created createTokens() method.

--- extra-javascript-stuff/04-practice/15-for-in-a-row/js/player.js
+++ extra-javascript-stuff/04-practice/15-for-in-a-row/js/player.js
@@ -4,6 +4,20 @@ class Player {
         this.id = id;
         this.color = color;
         this.active = active;
-        this.token = [];
+        this.token = this.createTokens(21);
+    }
+    /**
+     * Creates token objects for player
+     * @param     {number}    num - Number of token objects to be created
+     * @returns   {Array}     An array of the newly created token objects
+     */
+    createTokens(num) {
+        const tokens = [];
+
+        for (let i = 0; i < num; i++) {
+            let token = new Token(i, this);
+            tokens.push(token);
+        }
+        return tokens;
     }
 };
