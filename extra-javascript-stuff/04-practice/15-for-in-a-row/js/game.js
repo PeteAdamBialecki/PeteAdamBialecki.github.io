ce4a7e6f8995e1c068d43defe89a5eb5f2d4cf01
pbialecki_augeofi <pbialecki@augeofi.com>
2019-04-18 16:47:23 -0500

    Temp notes on game constructor and createPlatyers() method.

--- extra-javascript-stuff/04-practice/15-for-in-a-row/js/game.js
+++ extra-javascript-stuff/04-practice/15-for-in-a-row/js/game.js
@@ -1,1 +1,12 @@
-class Game {};
+class Game {
+    constructor() {
+        this.board = board;
+        this.players = this.createPlayers();
+        this.ready = false;
+    };
+    /**
+     * Creates two player objects
+     * @return  {Array}    An array of two Player objects.
+     */
+    createPlayers() {};
+};
