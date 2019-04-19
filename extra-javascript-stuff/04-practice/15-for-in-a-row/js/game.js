769f37402196539c4d11ebeca1e484b93406b3fe
pbialecki_augeofi <pbialecki@augeofi.com>
2019-04-18 20:42:03 -0500

    Created game constructor method and createPlayers function.

--- extra-javascript-stuff/04-practice/15-for-in-a-row/js/game.js
+++ extra-javascript-stuff/04-practice/15-for-in-a-row/js/game.js
@@ -8,5 +8,8 @@ class Game {
      * Creates two player objects
      * @return  {Array}    An array of two Player objects.
      */
-    createPlayers() {};
+    createPlayers() {
+        playerOne = [{name: 'Pete'}, {color : '#e15258'}, {id : 1}, {active : true} ],
+        playerTwo = [{name: 'Kye'}, {color : '#e599a13'}, {id : 2 }, {active : null} ]
+    };
 };
