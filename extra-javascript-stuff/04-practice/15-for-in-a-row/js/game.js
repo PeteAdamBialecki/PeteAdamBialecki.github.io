8d090d1acda5bcdbd99d73b8bb2612e5a692e7f5
pbialecki_augeofi <pbialecki@augeofi.com>
2019-04-23 18:19:17 -0500

    Created starter method.

--- extra-javascript-stuff/04-practice/15-for-in-a-row/js/game.js
+++ extra-javascript-stuff/04-practice/15-for-in-a-row/js/game.js
@@ -4,25 +4,18 @@ class Game {
         this.players = this.createPlayers();
         this.ready = false;
     };
-    
-    // Creates two player objects
-    // @return  {Array}    An array of two Player objects.
-
     get activePlayer() {
         return this.players.find(player => player.active);
     };
-
-    // createPlayers() {
-    //     playerOne = [{name: 'Pete'}, {color : '#e15258'}, {id : 1}, {active : true} ],
-    //     playerTwo = [{name: 'Kye'}, {color : '#e599a13'}, {id : 2 }, {active : null} ]
-    // };
-
     createPlayers() {
         const players = [
             new Player('Player 1', 1, '#e15258', true),
             new Player('Player 2', 2, '#e59a13')]; 
         return players;
     };
-    // Gets game ready for playing.
-    startGame() {};
+    startGame() {
+        this.board.drawHTMLBoard();
+        this.activePlayer.activeToken.drawHTMLToken();
+        this.ready = true;
+    };
 };
