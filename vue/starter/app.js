5462187e4f0ba577d2d9812db8d0b591e437e73c
pbialecki_augeofi <pbialecki@augeofi.com>
2019-06-13 20:11:03 -0500

    Added Vue img tag and loic.

--- vue/starter/app.js
+++ vue/starter/app.js
@@ -5,3 +5,16 @@ const helloWorld = new Vue ({
         message: "This is my first Vue project."
     }
 })
+
+const example = new Vue ({
+    el: '#example',
+    data: {
+        title: "Hello",
+        message: "Just sending out a signal, from one human to another",
+        name: "friend",
+        img: {
+            src: 'https://tau0.files.wordpress.com/2014/03/west_texas_cloudy_sun_bw.jpg',
+            alt: 'Gloomy Landscape'
+        }
+    }
+})
