9a26e9d33bcda0ef8e33cd5091f756670ac55134
pbialecki_augeofi <pbialecki@augeofi.com>
2019-06-20 20:11:23 -0500

    Connected image, summary, and title. Created starter template for increment and decrement functions.

--- vue/listing-rating/app.js
+++ vue/listing-rating/app.js
@@ -1,15 +1,18 @@
-// 1. Create a new Vue instance with the necessary options: el, data and methods. 
-    //Attach the new Vue instance to the div with the ID of "app". 
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Cassie',
+        summary: 'A golden retriever',
+        thumbnail: 'https://wallpaperstock.net/wallpapers/thumbs1/46221hd.jpg',
+        alt: 'golden retriever in the snow',
+        votes: '0'
+    },
+    methods: {
+        increment: function () {},
+        decrement: function () {}
+    }
+});
 
-// 2. Create the following properties on the data object: title, summary, thumbnail, alt, and votes. 
-//   Make up data for the "title" and "summary" properties. 
-//   For the thumbnail image, use a URl that links to your own photo or use this link to use a placeholder image: 
-//   'https://placeimg.com/75/75/any'. 
-//   The value of "votes" should be 0. 
-
-// 3. Use mustache syntax to display the vote count, title, and summary in the Vue template.
-
-// 4. Use v-bind to display the thumbnail image in the <img> tag. 
 
 // 5. On the Vue instance, create a method called "increment" that adds 1 vote to the "votes" property 
 
