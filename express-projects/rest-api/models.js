f4d4e61c59d8ed6556e1ed70dc31b9187151e4f3
pbialecki_augeofi <pbialecki@augeofi.com>
2019-07-01 20:10:21 -0500

    Answer schema methods.

--- express-projects/rest-api/models.js
+++ express-projects/rest-api/models.js
@@ -2,17 +2,43 @@
 
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+var sortAnswers = function(a, b) {
+    if(a.votes === b.votes) {
+        return b.updatedAt - a.updateAt;
+    }
+    return b.votes - a.votes;
+};
 var AnswerSchema = new Schema({
     text: String,
     createdAt: {type: Date, default: Date.now},
     updateAt: {type: Date, default: Date.now},
     votes: {type: Number, default: 0}
-})
-var QuestionSchema = new SVGSwitchElement({
+});
+
+AnswerSchema.method("update", function(updates, callback) {
+    Object.assign(this, updates, {updatedAt: new Date()});
+    this.parent().save(callback);
+});
+
+AnswerSchema.method("vote", function(vote, callback) {
+    if(vote == "up") {
+        this.votes += 1;
+    } else {
+        this.votes -= 1;
+    }
+});
+
+var QuestionSchema = new Schema({
     text: String,
     createdAt: {type: Date, default: Date.now},
     answer: [AnswerSchema]
 });
+
+QuestionSchema.prefix("save", function(next){
+    this.answers.sort(sortAnswers);
+    next();
+});
+
 var Question = mongoose.model("Question", QuestionSchema);
 
 module.exports.Question = Question;
