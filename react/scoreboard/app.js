97872c506a310ce3f576c9e000b813caa333f99d
pbialecki_augeofi <pbialecki@augeofi.com>
2019-07-07 13:01:03 -0500

    Divided into header, player, and counter components.

--- react/scoreboard/app.js
+++ react/scoreboard/app.js
@@ -11,18 +11,34 @@ const Player = () => {
     return (
         <div className="player">
             <span className="player-name">
-                Pete
+                Kye
             </span>
-            <div className="counter">
-                <button className="counter-action decrement"> - </button>
-                <span className="counter-score">35</span>
-                <button className="counter-action increment"> + </button>
-            </div>
+            <Counter />
+        </div>
+    );
+}
+
+const Counter = () => {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">35</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
+}
+
+const App = () => {
+    return (
+        <div className="scoreboard">
+            <Header />
+            {/* { Players } */}
+            <Player />
         </div>
     );
 }
 
 ReactDOM.render(
-    <Player />,
+    <App />,
     document.getElementById('root')
 );
