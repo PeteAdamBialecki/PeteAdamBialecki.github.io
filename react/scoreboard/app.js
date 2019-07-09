826a4fac8ea34108420c784580062945da70b88b
pbialecki_augeofi <pbialecki@augeofi.com>
2019-07-08 20:14:28 -0500

    Added initial players props map.

--- react/scoreboard/app.js
+++ react/scoreboard/app.js
@@ -1,8 +1,36 @@
+const players = [
+    {
+        name: "Kye",
+        score: 98,
+        id: 1
+    },
+    {
+        name: "Phil",
+        score: 50,
+        id: 2
+    },
+    {
+        name: "Eva",
+        score: 85,
+        id: 3
+    },
+    {
+        name: "Ara",
+        score: 95,
+        id: 4
+    },
+    {
+        name: "Pete",
+        score: 25,
+        id: 5
+    }
+];
+
 function Header(props) {
     return (
         <header>
-            <h1>{ props.title }</h1>
-            <span className="stats">Players: { props.totalPlayers }</span>
+            <h1>{props.title}</h1>
+            <span className="stats">Players: {props.totalPlayers}</span>
         </header>
     );
 }
@@ -11,9 +39,9 @@ const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
-                { props.name }
+                {props.name}
             </span>
-            <Counter score={ props.score }/>
+            <Counter score={props.score} />
         </div>
     );
 }
@@ -22,24 +50,32 @@ const Counter = (props) => {
     return (
         <div className="counter">
             <button className="counter-action decrement"> - </button>
-            <span className="counter-score">{ props.score }</span>
+            <span className="counter-score">{props.score}</span>
             <button className="counter-action increment"> + </button>
         </div>
     );
 }
 
-const App = () => {
+const App = (props) => {
     return (
         <div className="scoreboard">
-            <Header title="My Scoreboard"
-            totalPlayers={ 11 }/>
+            <Header
+                title="My Scoreboard"
+                totalPlayers={ props.initialPlayers.length }
+            />
             {/* { Players } */}
-            <Player name="Pete" score={ 50 }/>
+            { props.initialPlayers.map( player => 
+                <Player
+                name={ player.name}
+                score={ player.score }
+                key={ player.id.toString() }
+                />
+            )}
         </div>
     );
 }
 
 ReactDOM.render(
-    <App />,
+    <App initialPlayers={ players }/>,
     document.getElementById('root')
 );
