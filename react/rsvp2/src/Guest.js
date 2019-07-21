3b031b585182659628f8099a323d1275191cfbcc
pbialecki_augeofi <pbialecki@augeofi.com>
2019-07-21 15:27:11 -0500

    Added the toggleConfirmationAt ability.

--- react/rsvp2/src/Guest.js
+++ react/rsvp2/src/Guest.js
@@ -5,15 +5,19 @@ const Guest = props =>
     <li>
         <span>{props.name}</span>
         <label>
-            <input type="checkbox" checked={props.isConfirmed} /> Confirmed
-                </label>
+            <input 
+            type="checkbox" 
+            checked={props.isConfirmed} 
+            onChange={props.handleConfirmation} /> Confirmed
+        </label>
         <button>edit</button>
         <button>remove</button>
-    </li>
+    </li>;
 
 Guest.propTypes = {
     name: PropTypes.string.isRequired,
-    isConfirmed: PropTypes.bool.isRequired
+    isConfirmed: PropTypes.bool.isRequired,
+    handleConfirmation: PropTypes.func.isRequired
 };
 
 export default Guest;
