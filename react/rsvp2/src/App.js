f3a8d33668824073892c0f1cc7513b9e494f4296
pbialecki_augeofi <pbialecki@augeofi.com>
2019-07-21 14:31:25 -0500

    Started GuestList.js.  Updated enviornment.

--- react/rsvp2/src/App.js
+++ react/rsvp2/src/App.js
@@ -1,89 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import GuestList from './GuestList';
 
-function App() {
+class App extends Component {
 
-    state = {
-        guests: [
-            {
-                name: 'Kye',
-                isConfirmed: false,
+  state = {
+    guests: [
+      {
+        name: 'Phil',
+        isConfirmed: false
+      },
+      {
+        name: 'Kye',
+        isConfirmed: true
+      },
+      {
+        name: 'Eva',
+        isConfirmed: false
+      }
+    ]
+  };
 
-            },
-            {
-                name: 'Eva',
-                isConfirmed: true,
+  getTotalInvited = () => this.state.guests.length;
+  // getAttendingGuests = () =>
+  // getUnconfirmedGuests = () =>
 
-            }
-        ]
-    }
-
-    getTotalInvited = () => this.state.guests.length;
-    // getAttendingGuests = () => 
-    // getUnconfirmedGuests = () => 
-
+  render() {
     return (
-        <div className="App">
-            <header>
-                <h1>RSVP</h1>
-                <p>A Treehouse App</p>
-                <form>
-                    <input type="text" value="Safia" placeholder="Invite Someone" />
-                    <button type="submit" name="submit" value="submit">Submit</button>
-                </form>
-            </header>
-            <div className="main">
-                <div>
-                    <h2>Invitees</h2>
-                    <label>
-                        <input type="checkbox" />Hide those who haven't responded
-          </label>
-                </div>
-                <table className="counter">
-                    <tbody>
-                        <tr>
-                            <td>Attending:</td>
-                            <td>2</td>
-                        </tr>
-                        <tr>
-                            <td>Unconfirmed:</td>
-                            <td>1</td>
-                        </tr>
-                        <tr>
-                            <td>Total:</td>
-                            <td>3</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <ul>
-                    <li className="pending"><span>Safia</span></li>
-                    <li className="responded"><span>Iver</span>
-                        <label>
-                            <input type="checkbox" checked /> Confirmed
+      <div className="App">
+        <header>
+          <h1>RSVP</h1>
+          <p>A Treehouse App</p>
+          <form>
+            <input type="text" value="Safia" placeholder="Invite Someone" />
+            <button type="submit" name="submit" value="submit">Submit</button>
+          </form>
+        </header>
+        <div className="main">
+          <div>
+            <h2>Invitees</h2>
+            <label>
+              <input type="checkbox" /> Hide those who haven't responded
             </label>
-                        <button>edit</button>
-                        <button>remove</button>
-                    </li>
-                    <li className="responded">
-                        <span>Corrina</span>
-                        <label>
-                            <input type="checkbox" checked /> Confirmed
-            </label>
-                        <button>edit</button>
-                        <button>remove</button>
-                    </li>
-                    <li>
-                        <span>Joel</span>
-                        <label>
-                            <input type="checkbox" /> Confirmed
-            </label>
-                        <button>edit</button>
-                        <button>remove</button>
-                    </li>
-                </ul>
-            </div>
+          </div>
+          <table className="counter">
+            <tbody>
+              <tr>
+                <td>Attending:</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>Unconfirmed:</td>
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>Total:</td>
+                <td>3</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <GuestList guests={this.state.guests}/>
+
         </div>
+      </div>
     );
+  }
 }
 
 export default App;
