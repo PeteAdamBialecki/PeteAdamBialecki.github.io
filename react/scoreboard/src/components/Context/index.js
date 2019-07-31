bf2348fa84bfba2092972d0def35d18f06e991ad
pbialecki_augeofi <pbialecki@augeofi.com>
2019-07-30 19:59:19 -0500

    Introduced Provider + Consumer.  Cleaned up Header.js.

---
+++ react/scoreboard/src/components/Context/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ScoreBoardContext = React.createContext();
+export const Provider = ScoreBoardContext.Provider;
+export const Consumer = ScoreBoardContext.Consumer;
