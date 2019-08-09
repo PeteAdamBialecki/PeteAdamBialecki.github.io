f27aa6d30fe77b527c4f7324a61147f7aaf51f8f
pbialecki_augeofi <pbialecki@augeofi.com>
2019-08-08 19:48:25 -0500

    Built 404 error route.

--- react/router-4/sample-course-website/src/components/App.js
+++ react/router-4/sample-course-website/src/components/App.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 
 import {
     BrowserRouter,
-    Route
+    Route,
+    Switch
 } from 'react-router-dom';
 
 // App Components
@@ -12,15 +13,19 @@ import Home from './Home';
 import About from './About';
 import Teachers from './Teachers';
 import Courses from './Courses';
+import NotFound from './NotFound';
 
 const App = () => (
     <BrowserRouter>
         <div className="container">
             <Header />
-            <Route exact path="/" component={Home} />
-            <Route path="/about" render={ () => <About title='About'/> } />
-            <Route path="/teachers" component={Teachers} />
-            <Route path="/courses" component={Courses} />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" render={() => <About title='About' />} />
+                <Route path="/teachers" component={Teachers} />
+                <Route path="/courses" component={Courses} />
+                <Route component={NotFound} />
+            </Switch>
         </div>
     </BrowserRouter>
 );
