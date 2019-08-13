c8062b7e83d4df90be470f312c2967c2f3a47d15
pbialecki_augeofi <pbialecki@augeofi.com>
2019-08-12 20:19:11 -0500

    Initial GIPHY API respponse set up.

--- react/gif-search/src/App.js
+++ react/gif-search/src/App.js
@@ -7,9 +7,24 @@ export default class App extends Component {
   
   constructor() {
     super();
-  } 
+    this.state = {
+        gifs: []
+    };
+  }
 
-  render() { 
+  componentDidMount() {
+      fetch('http://api.giphy.com/v1/gifs/trending?api_key=V2Nr5sIitb8Ooqbfn5KsJlwjuPe7Ha8W')
+      .then(response => response.json())
+      .then(responseData => {
+          this.setState({ gifs: responseData.data });
+      })
+      .catch(error => {
+          console.log('Error fetching and parsing data:', error);
+      });
+  }
+
+  render() {
+      console.log(this.state.gifs); 
     return (
       <div>
         <div className="main-header">
