c34b60d03c804fbd11b57a09029c0d740d9da880
pbialecki_augeofi <pbialecki@augeofi.com>
2019-09-10 18:28:14 -0500

    Update player switch case.

--- react/redux/scoreboard/src/reducers/player.js
+++ react/redux/scoreboard/src/reducers/player.js
@@ -1,57 +1,80 @@
 import * as PlayerActionTypes from '../actiontypes/player';
 
 const initialState = {
-	players: [{
-		name: 'Kye Kim',
-	  score: 31,
-		created: '11/8/2016',
-		updated: '11/9/2016'
-	},
-	{
-		name: 'Eva Bialecki',
-		score: 20,
-		created: '11/9/2016',
-		updated: '11/10/2016'
-	},
-	{
-		name: 'Phil Hardman',
-		score: 50,
-		created: '11/11/2016',
-		updated: '11/12/2016'
-	}
-	],
-	selectedPlayerIndex: -1
+    players: [{
+        name: 'Kye Kim',
+        score: 31,
+        created: '11/8/2016',
+        updated: '11/9/2016'
+    },
+    {
+        name: 'Eva Bialecki',
+        score: 20,
+        created: '11/9/2016',
+        updated: '11/10/2016'
+    },
+    {
+        name: 'Phil Hardman',
+        score: 50,
+        created: '11/11/2016',
+        updated: '11/12/2016'
+    }
+    ],
+    selectedPlayerIndex: -1
 }
 
-export default function Player(state=initialState, action) {
-  switch(action.type) {
-    case PlayerActionTypes.ADD_PLAYER:
-      return [
-        ...state,
-        {
-          name: action.name,
-          score: 0
+export default function Player(state = initialState, action) {
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    switch (action.type) {
+        case PlayerActionTypes.ADD_PLAYER:
+            const addPlayerLilst = [...state.players, {
+                name: action.name,
+                score: 0,
+                created: `${month}/${day}/${year}`
+            }];
+            return {
+                ...state,
+                players: addPlayerList
+            };
+
+        case PlayerActionTypes.REMOVE_PLAYER: {
+            const removePlayerList = [
+                ...state.slice(0, action.index),
+                ...state.slice(action.index + 1)
+            ];
+            return [
+                ...state,
+                players: removePlayerList
+            ];
         }
-      ];
-      
-    case PlayerActionTypes.REMOVE_PLAYER:
-      return [
-        ...state.slice(0, action.index),
-        ...state.slice(action.index + 1)
-      ];
-      
-    case PlayerActionTypes.UPDATE_PLAYER_SCORE:
-      return state.map((player, index) => {
-        if(index === action.index) {
-          return {
-            ...player,
-            score: player.score + action.score
-          };
+
+        case PlayerActionTypes.UPDATE_PLAYER_SCORE: {
+            const updatePlayerList = state.players.map((player, index) => {
+                if (index === action.index) {
+                    return {
+                        ...player,
+                        score: player.score + action.score,
+                        updated: `${month}/${day}/${year}`
+                    };
+                }
+                return player;
+            });
+            return {
+                ...state,
+                players: updatePlayerList
+            };
         }
-        return player;
-      });
-      
-    default:
-      return state;
-  }
+
+        case PlayerActionTypes.SELECT_PLAYER:
+            return {
+                ...state,
+                selectedPlayerIndex: action.index
+            };
+
+        default:
+            return state;
+    }
 }
