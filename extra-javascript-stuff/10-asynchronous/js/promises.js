842bd8b1c05c21bbbad036c23836e5e58a2b4e1b
pbialecki_augeofi <pbialecki@augeofi.com>
2019-09-14 15:46:54 -0500

    Converted getProfiles and onClick to promise.

--- extra-javascript-stuff/10-asynchronous/js/promises.js
+++ extra-javascript-stuff/10-asynchronous/js/promises.js
@@ -3,28 +3,34 @@ const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
-function getJSON(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = () => {
-    if(xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
-      return callback(data);
-    }
-  };
-  xhr.send();
+function getJSON(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                resolve(data);
+            } else {
+                reject( Error(xhr.statusText));
+            }
+        };
+        xhr.onerror = () => reject(Error('A network error occured'));
+        xhr.send();
+    });
 }
 
 function getProfiles(json) {
-  json.people.map( person => {
-    getJSON(wikiUrl + person.name, generateHTML);      
-  }); 
+    const profiles = json.people.map(person => {
+        return getJSON(wikiUrl + person.name);
+    });
+    return profiles;
 }
 
 function generateHTML(data) {
-  const section = document.createElement('section');
-  peopleList.appendChild(section);
-  section.innerHTML = `
+    const section = document.createElement('section');
+    peopleList.appendChild(section);
+    section.innerHTML = `
     <img src=${data.thumbnail.source}>
     <h2>${data.title}</h2>
     <p>${data.description}</p>
@@ -33,6 +39,9 @@ function generateHTML(data) {
 }
 
 btn.addEventListener('click', (event) => {
-  getJSON(astrosUrl, getProfiles);
-  event.target.remove();
+    getJSON(astrosUrl)
+        .then(getProfiles)
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+    event.target.remove();
 });
