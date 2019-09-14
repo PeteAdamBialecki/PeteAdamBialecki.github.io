8c92cce141620c97e369971d64977ff3fb09fe8c
pbialecki_augeofi <pbialecki@augeofi.com>
2019-09-14 14:20:21 -0500

    Implemented astrosUrl call.

--- extra-javascript-stuff/10-asynchronous/js/callbacks.js
+++ extra-javascript-stuff/10-asynchronous/js/callbacks.js
@@ -4,26 +4,37 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // Make an AJAX request
-function getJSON(url) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = () => {
-    if(xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
-      console.log(data);
-    }
-  };
-  xhr.send();
+function getJSON(url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            let data = JSON.parse(xhr.responseText);
+            return callback(data);
+        }
+    };
+    xhr.send();
+}
+
+function getProfiles(json) {
+    json.people.map(person => {
+        getJSON(wikiUrl + person.name, generateHTML);
+    });
 }
 
 // Generate the markup for each profile
 function generateHTML(data) {
-  const section = document.createElement('section');
-  peopleList.appendChild(section);
-  section.innerHTML = `
+    const section = document.createElement('section');
+    peopleList.appendChild(section);
+    section.innerHTML = `
     <img src=${data.thumbnail.source}>
     <h2>${data.title}</h2>
     <p>${data.description}</p>
-    <p>${data.extract}</p>
+    <p>${data.extract}</p> 
   `;
 }
+
+btn.addEventListener('click', () => {
+    getJSON(astrosUrl, getProfiles);
+    event.target.remove();
+});
