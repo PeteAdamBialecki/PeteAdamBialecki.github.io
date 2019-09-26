698e69d75b974db54d3e269de40c890a7bd19b58
pbialecki_augeofi <pbialecki@augeofi.com>
2019-09-25 19:57:01 -0500

    Structrual directive ffor 'like' button and hide/show comments.

--- angular/photo-blog/src/app/entries/entry/entry.component.ts
+++ angular/photo-blog/src/app/entries/entry/entry.component.ts
@@ -10,4 +10,9 @@ export class EntryComponent {
     title: string = 'My first photo.';
     photo: string = 'http://placehold.it/800x500?text=Angular Basics';
     description: string = 'A description and experiment for the future...';
+    comments: any[] = [
+        {name: "Casper", comment: "A comment"},
+        {name: "Arnold", comment: "A comment"},
+        {name: "Obi Wan", comment: "A comment"}
+    ]
 }
