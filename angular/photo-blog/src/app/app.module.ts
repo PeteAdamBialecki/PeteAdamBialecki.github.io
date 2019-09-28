07c197623b7c57a46b714d922d32e43fef6282a6
pbialecki_augeofi <pbialecki@augeofi.com>
2019-09-28 08:21:16 -0500

    Sample front/back end EntryService .

--- angular/photo-blog/src/app/app.module.ts
+++ angular/photo-blog/src/app/app.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { EntryListComponent, EntryComponent } from './entries';
+import { EntryListComponent, EntryComponent, EntryService } from './entries';
 
 @NgModule({
-    imports: [BrowserModule, HttpModule],
+    imports: [
+        BrowserModule, 
+        HttpModule
+    ],
+    providers: [
+        EntryService
+    ],
     declarations: [
         AppComponent,
         EntryComponent,
         EntryListComponent
     ],
-    bootstrap: [AppComponent]
+    bootstrap: [
+        AppComponent
+    ]
 })
 export class AppModule {
 
--- angular/photo-blog/src/app/entries/entry/entry.component.ts
+++ angular/photo-blog/src/app/entries/entry/entry.component.ts
@@ -5,14 +5,3 @@ import { Component } from '@angular/core';
     templateUrl: 'entry.component.html',
     styleUrls: ['entry.component.css']
 })
-
-export class EntryComponent {
-    title: string = 'My first photo.';
-    photo: string = 'http://placehold.it/800x500?text=Angular Basics';
-    description: string = 'A description and experiment for the future...';
-    comments: any[] = [
-        {name: "Casper", comment: "A comment"},
-        {name: "Arnold", comment: "A comment"},
-        {name: "Obi Wan", comment: "A comment"}
-    ]
-}
