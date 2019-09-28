ec1bd443d3beabc2cb766270bf7357d05d5552e5
pbialecki_augeofi <pbialecki@augeofi.com>
2019-09-28 10:01:07 -0500

    Added comment form.

--- angular/photo-blog/src/app/app.module.ts
+++ angular/photo-blog/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { FormsModule }  from '@angular/forms';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppComponent } from './app.component';
-import { EntryListComponent, EntryComponent, EntryService } from './entries';
+import { EntryListComponent, EntryComponent, EntryService, EntryCommentFormComponent } from './entries';
 import { InMemoryEntryService } from './backend';
 
 
@@ -11,6 +12,7 @@ import { InMemoryEntryService } from './backend';
     imports: [
         BrowserModule, 
         HttpModule,
+        FormsModule,
         InMemoryWebApiModule.forRoot(InMemoryEntryService)
     ],
     providers: [
@@ -19,7 +21,8 @@ import { InMemoryEntryService } from './backend';
     declarations: [
         AppComponent,
         EntryComponent,
-        EntryListComponent
+        EntryListComponent,
+        EntryCommentFormComponent
     ],
     bootstrap: [
         AppComponent
