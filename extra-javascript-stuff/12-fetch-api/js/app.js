0f389ada469c5f645c529e287610b44fb44ed722
pbialecki_augeofi <pbialecki@augeofi.com>
2019-10-05 17:02:57 -0500

    Sample fetch data posting.

--- extra-javascript-stuff/12-fetch-api/js/app.js
+++ extra-javascript-stuff/12-fetch-api/js/app.js
@@ -1,6 +1,6 @@
 const select = document.getElementById('breeds');
 const card = document.querySelector('.card');
-const form = document.querySelector('form'); 
+const form = document.querySelector('form');
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
@@ -13,18 +13,22 @@ function fetchData(url) {
         .catch(error => console.log("I think there was a problem", error))
 }
 
-fetchData('https://dog.ceo/api/breeds/list')
-    .then(data => generateOptions(data.message))
-
-fetchData('https://dog.ceo/api/breeds/image/random')
-    .then(data => generateImage(data.message))
+Promise.all([
+    fetchData(('https://dog.ceo/api/breeds/list')),
+    fetchData('https://dog.ceo/api/breeds/image/random')
+]).then(data => {
+    const breedList = data[0].message;
+    const randomImage = data[1].message
+    generateOptions(breedList);
+    generateImage(randomImage);
+})
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
 
 function checkStatus(response) {
-    if(response.ok) {
+    if (response.ok) {
         return Promise.resolve(response);
     } else {
         return Promise.reject(new Error(response.statusText));
@@ -65,8 +69,26 @@ function fetchBreedImage() {
 
 select.addEventListener('change', fetchBreedImage);
 card.addEventListener('click', fetchBreedImage);
+form.addEventListener('submit', postData);
 
 // ------------------------------------------
 //  POST DATA
 // ------------------------------------------
 
+function postData(e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const comment = document.getElementById('comment').value;
+    const config = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, comment })
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/comments', config)
+        .then(checkStatus)
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
