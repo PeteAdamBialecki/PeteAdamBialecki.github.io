4b7acc6595e89c777237515e2e6f39b7be1b792d
pbialecki_augeofi <pbialecki@augeofi.com>
2019-10-05 12:18:19 -0500

    Random functionality and content generation based on breed.

--- extra-javascript-stuff/12-fetch-api/js/app.js
+++ extra-javascript-stuff/12-fetch-api/js/app.js
@@ -1,19 +1,21 @@
 const select = document.getElementById('breeds');
-const card = document.querySelector('.card'); 
+const card = document.querySelector('.card');
 const form = document.querySelector('form');
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
 
-fetch('https://dog.ceo/api/breeds/list')
-    .then(response => response.json())
+function fetchData(url) {
+    return fetch(url)
+        .then(res => res.json())
+}
+
+fetchData('https://dog.ceo/api/breeds/list')
     .then(data => generateOptions(data.message))
 
-fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
+fetchData('https://dog.ceo/api/breeds/image/random')
     .then(data => generateImage(data.message))
-
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
@@ -28,18 +30,31 @@ function generateOptions(data) {
 
 function generateImage(data) {
     const html = `
-        <img src='${data}' alt>
-        <p>Click to view images of ${select.value}s</p>
-    `;
+    <img src='${data}' alt>
+    <p>Click to view images of ${select.value}s</p>
+  `;
     card.innerHTML = html;
 }
 
+function fetchBreedImage() {
+    const breed = select.value;
+    const img = card.querySelector('img');
+    const p = card.querySelector('p');
+
+    fetchData(`https://dog.ceo/api/breed/${breed}/images/random`)
+        .then(data => {
+            img.src = data.message;
+            img.alt = breed;
+            p.textContent = `Click to view more ${breed}s`;
+        })
+}
 
 // ------------------------------------------
 //  EVENT LISTENERS
 // ------------------------------------------
 
-
+select.addEventListener('change', fetchBreedImage);
+card.addEventListener('click', fetchBreedImage);
 
 // ------------------------------------------
 //  POST DATA
