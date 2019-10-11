ff3b3f68757c92e159be5ea920bfa260e822fe85
pbialecki_augeofi <pbialecki@augeofi.com>
2019-10-11 12:51:20 -0500

    Reorganized assets and code.

--- node-projects/sql-orm-node/app.js
+++ node-projects/sql-orm-node/app.js
@@ -1,42 +1,20 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: 'movies.db',
-    logging: true  // disable logging
-});
+const db = require('./db');
+const { Movie } = db.models;
 
-// Movie model
-class Movie extends Sequelize.Model { }
-Movie.init({
-    title: Sequelize.STRING
-}, { sequelize }); // same as { sequelize: sequelize }
-
-// async IIFE
 (async () => {
-    // Sync 'Movies' table
-    await sequelize.sync({ force: true });
-    try {
-        // non-Refactoered
-        // const movie1 = await Movie.create({
-        //     title: 'Samsara',
-        // });
-        // const movie2 = await Movie.create({
-        //     title: '2001: A Space Odyssey',
-        // });
-        // console.log(movie2.toJSON());
-        // Refactored
-        const movieInstances = await Promise.all([
-            Movie.create({
-                title: 'Samsara',
-            }),
-            Movie.create({
-                title: '2001: A Space Odyssey',
-            }),
-        ]);
-        const moviesJSON = movieInstances.map(movie => movie.toJSON());
-        console.log(moviesJSON);
+    await db.sequelize.sync({ force: true });
 
-        // ------------------
+    try {
+        const movie = await Movie.create({
+            title: 'Samsara'
+        });
+        console.log(movie.toJSON());
+
+        const movie2 = await Movie.create({
+            title: '2001: A Space Odyssey'
+        });
+        console.log(movie2.toJSON());
+
     } catch (error) {
         console.error('Error connecting to the database: ', error);
     }
