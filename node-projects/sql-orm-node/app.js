25fc002f80ca4db06d0af70eaf0d40e453bc3459
pbialecki_augeofi <pbialecki@augeofi.com>
2019-10-11 13:38:21 -0500

    Additional properties.

--- node-projects/sql-orm-node/app.js
+++ node-projects/sql-orm-node/app.js
@@ -6,12 +6,18 @@ const { Movie } = db.models;
 
     try {
         const movie = await Movie.create({
-            title: 'Samsara'
+            title: 'Samsara',
+            runtime: 99,
+            releaseDate: '2011-09-11',
+            isAvailableOnVHS: false,
         });
         console.log(movie.toJSON());
 
         const movie2 = await Movie.create({
-            title: '2001: A Space Odyssey'
+            title: '2001: A Space Odyssey',
+            runtime: 142,
+            releaseDate: '1968-04-03',
+            isAvailableOnVHS: true,
         });
         console.log(movie2.toJSON());
 
--- node-projects/sql-orm-node/db/models/movie.js
+++ node-projects/sql-orm-node/db/models/movie.js
@@ -3,7 +3,29 @@ const Sequelize = require('sequelize');
 module.exports = (sequelize) => {
     class Movie extends Sequelize.Model { }
     Movie.init({
-        title: Sequelize.STRING,
+        // Set custom primary key column
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: Sequelize.STRING,
+            allowNull: false, // disallow null
+        },
+        runtime: {
+            type: Sequelize.INTEGER,
+            allowNull: false, // disallow null
+        },
+        releaseDate: {
+            type: Sequelize.DATEONLY,
+            allowNull: false, // disallow null
+        },
+        isAvailableOnVHS: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false, // disallow null
+            defaultValue: false, // set default value
+        },
     }, { sequelize });
 
     return Movie;
