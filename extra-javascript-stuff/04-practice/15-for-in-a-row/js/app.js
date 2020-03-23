5bc296fa888f088fffa6c699ac3b37b5bebee451
pbialecki_augeofi <pbialecki@augeofi.com>
2020-03-22 21:19:44 -0500

    Connect 4 funtioncal.

--- extra-javascript-stuff/04-practice/15-for-in-a-row/js/app.js
+++ extra-javascript-stuff/04-practice/15-for-in-a-row/js/app.js
@@ -2,6 +2,7 @@ const game = new Game();
 document.getElementById('begin-game').addEventListener('click', function () {
     game.startGame();
     this.style.display = 'none';
+    document.getElementById('game-intro').style.display = 'none';
     document.getElementById('play-area').style.opacity = '1';
     document.getElementById("table").style.display = "block";
 });
